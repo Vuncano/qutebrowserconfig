39047b910385e00aa739235e786b05ead95b88ee
vuncano <vuncano18@gmail.com>
2024-09-20 14:19:12 -0300

    commit

--- config.py
+++ config.py
@@ -17,12 +17,12 @@ config.bind('K', 'tab-next')
 c.colors.webpage.preferred_color_scheme = 'dark'
 c.colors.webpage.darkmode.algorithm = 'lightness-cielab'
 c.colors.webpage.darkmode.contrast = 0.0
-c.colors.webpage.darkmode.grayscale.all = False
-c.colors.webpage.darkmode.grayscale.images = 0.0
-c.colors.webpage.darkmode.policy.images = 'smart'
+# c.colors.webpage.darkmode.grayscale.all = False
+# c.colors.webpage.darkmode.grayscale.images = 0.0
+c.colors.webpage.darkmode.policy.images = 'smart-simple'
 c.colors.webpage.darkmode.policy.page = 'smart'
 c.colors.webpage.darkmode.threshold.background = 128
-c.colors.webpage.darkmode.threshold.text = 128 
+c.colors.webpage.darkmode.threshold.foreground = 128
 # c.colors.webpage.darkmode.enabled = True
 
 
